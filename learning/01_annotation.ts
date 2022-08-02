cad14fee11957dc5f345578f8b3708861f056f7b
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-08-02 12:28:30 +0545

    Array and object literals

--- learning/01_annotation.ts
+++ learning/01_annotation.ts
@@ -6,3 +6,21 @@ let nothing:undefined =undefined;
 
 // built in object
 let now:Date = new Date();
+
+// Array
+let colors: string[] = ['red','green','blue']
+let numbers:number[] =[2,3,4,5]
+let truths:boolean[]=[true,false,false,true]
+
+// class
+class Car {
+
+}
+let car:Car = new Car();
+
+//object literals
+let point:{x:number; y:number; z:string} = {
+    x:10,
+    y:20,
+    z:'apple',
+}
