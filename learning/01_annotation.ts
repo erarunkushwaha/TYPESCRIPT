73098c3ed2c16d5616a5dfacf178ff26ca32fad4
Arun Kushwaha <er.arunkushwaha@gmail.com>
2022-08-02 12:14:59 +0545

    annotation

--- learning/01_annotation.ts
+++ learning/01_annotation.ts
@@ -1,1 +1,8 @@
-console.log('i am here ')
+let apple:string ="i have  apple"
+let total:number = 4;
+let whiteApple:boolean = false;
+let nothingMuch:null = null;
+let nothing:undefined =undefined;
+
+// built in object
+let now:Date = new Date();
