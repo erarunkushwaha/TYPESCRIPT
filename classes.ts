7ba8e0d6bfe62e1516e21eea398deb1e3ef1d756
Arun Kushwaha <er.arunkushwaha@gmail.com>
2023-02-04 19:39:21 +0545

    clacces introduced

--- classes.ts
+++ classes.ts
@@ -1,12 +1,22 @@
-class Vehicles{
-    drive():void{
-        console.log('Drive slowly')
-    }
+class Vehicles {
+  private drive(): void {
+    console.log("Drive slowly");
+  }
 
-     speed():void{
-        console.log('Keep speed below 20km/hr')
-    }
+  speed(): void {
+    console.log("Keep speed below 20km/hr");
+  }
 }
 
-const vechile = new Vehicles()
-vechile.speed()
+const vechile = new Vehicles();
+vechile.speed();
+
+class NewCar extends Vehicles {
+  // @ovveride
+  speed(): void {
+    console.log("Keep speed below 2000km/hr");
+  }
+}
+
+const callCar = new Vehicles();
+// callCar.drive() -->> error
