856cd395277a6aaf08124d0514d7759d1f121194
Arun Kushwaha <er.arunkushwaha@gmail.com>
2023-02-04 21:56:02 +0545

    google maps added

--- MAPS/src/index.ts
+++ MAPS/src/index.ts
@@ -1,6 +1,18 @@
+import { Loader } from "@googlemaps/js-api-loader";
 import { Users } from "./User";
 import { Company } from "./Company";
 
 const users = new Users();
 const company = new Company();
-console.log(users);
+const loader = new Loader({
+  apiKey: "AIzaSyAjqq8XtzcCV8W3HLCODbCIlStjeh7cdJg",
+  version: "weekly"
+});
+
+loader.load().then(() => {
+  let map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+});
+// new google.maps.Map(document.getElementById("mps"));
