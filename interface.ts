c71bb70fe0a756c42da0c552524676da885a4444
Arun Kushwaha <er.arunkushwaha@gmail.com>
2023-02-04 19:08:05 +0545

    interface introduced

--- interface.ts
+++ interface.ts
@@ -1,21 +1,33 @@
-interface Vehicle{
-    name:string,
-    year:number,
-    borken:boolean
-    summary():string
+interface Repotable {
+  summary(): string;
+}
+
+interface Vehicle extends Repotable {
+  name: string;
+  year: number;
+  borken: boolean;
+}
+
+interface OldDring extends Repotable {
+  name: string;
+  year: number;
 }
 
 const oldVehicle = {
-    name:'civic',
-    year:2000,
-    borken:false,
-    summary():string{
-return `name ${this.name}`
-    }
-}
+  name: "civic",
+  year: 2000,
+  borken: false,
+  summary(): string {
+    return `name ${this.name}`;
+  }
+};
 
-const printVehicle = (Vehicle:Vehicle):void => {
-    console.log(Vehicle)
+const oldDring = {
+  name: "cocktail",
+  color: "red"
+};
 
-}
-printVehicle(oldVehicle)
+const printVehicle = (Vehicle: Vehicle): void => {
+  console.log(Vehicle.summary);
+};
+printVehicle(oldVehicle);
